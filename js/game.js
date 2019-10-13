b3dd344de0bb3703bf44f24b9579b9d7588b7574
Argin Aslanian <someone@somewhere.com>
2019-10-13 11:01:37 -0700

    Added enemy.js and player.js. Game loading on the browser.

--- js/game.js
+++ js/game.js
@@ -28,10 +28,12 @@ var game = {
     // Run on game resources loaded.
     "loaded" : function () {
         // me.state.set(me.state.MENU, new game.TitleScreen());
+        me.pool.register("player", game.Player);
+        me.pool.register("enemy", game.Enemy);
         me.state.set(me.state.PLAY, new game.PlayScreen());
 
         // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
+        //me.pool.register("mainPlayer", game.PlayerEntity);
 
         // Start the game.
         me.state.change(me.state.PLAY);
